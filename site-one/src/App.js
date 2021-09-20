c0ac90dd54e7562e0870452606bb7a4a1c6430fd
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-20 20:10:26 +1000

    update on the functionality

--- site-one/src/App.js
+++ site-one/src/App.js
@@ -7,9 +7,26 @@ const [count, setCount] = useState(0);
         <div style={{textAlign:"center"}}>
             <h1> Hello this is the hotel number of customer booking counter</h1>
         <h2> {count} </h2>
-         <button onClick = {()=>{setCount(count)}}>Reset</button>
-        <button onClick={()=>{setCount(count+1)}}>Increase</button>
-        <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+         <button onClick = {()=>{setCount(0)}}>Reset</button>
+        <button onClick={()=>{
+            if(count>=10){
+                alert("you have reached the max value")
+            }
+            else{
+            setCount(count+1)}}
+        }>
+                
+                
+                Increase</button>
+
+
+
+        <button onClick={()=>{
+            if(count<=0){
+                alert("you have reached the lowest value as possible")
+            }
+            else{
+            setCount(count-1)}}}>Decrease</button>
        
         </div>
     )
