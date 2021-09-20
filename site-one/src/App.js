082ff473376deaca4ca72ffe2617beda8dc81d07
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-20 19:59:04 +1000

    update to Ap.js

---
+++ site-one/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const App = () => {
+    
+const [count, setCount] = useState(0);
+    return (
+        <div style={{textAlign:"center"}}>
+            <h1> Hello this is the hotel number of customer booking counter</h1>
+        <h2> {count} </h2>
+        <button onClick={()=>{setCount(count+1)}}>Increase</button>
+        </div>
+    )
+}
+
+export default App
