ab8771d359d6a67648d2e771193f60d3a6d40f58
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-20 20:03:30 +1000

    App.js
    The button increment has been updated

--- site-one/src/App.js
+++ site-one/src/App.js
@@ -7,7 +7,10 @@ const [count, setCount] = useState(0);
         <div style={{textAlign:"center"}}>
             <h1> Hello this is the hotel number of customer booking counter</h1>
         <h2> {count} </h2>
+         <button onClick = {()=>{setCount(count)}}>Reset</button>
         <button onClick={()=>{setCount(count+1)}}>Increase</button>
+        <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+       
         </div>
     )
 }
