9af9a6ffc8ca0546782e35fbf35ef9ddc14ecb31
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-21 18:49:42 +1000

    raw

--- site-one/src/App.js
+++ site-one/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+    const [count, setCount] =useState(0)
     return (
         <div>
            <h1> Hello this is our freshly cooked hotel customer counter machine</h1>
+       <h2> {count}</h2>
+       <button onClick={()=>{setCount(count+1)}}> Increment Count</button>
+       <button onClick={()=>{setCount(count-1)}}> Increment Count</button>
+       <button> Reset</button>
         </div>
     )
 }
