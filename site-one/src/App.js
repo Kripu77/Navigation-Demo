48695b4e8829729b32a773aa1ba68788b2446712
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-21 18:45:35 +1000

    update file

---
+++ site-one/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+           <h1> Hello this is our freshly cooked hotel customer counter machine</h1>
+        </div>
+    )
+}
+
+export default App
