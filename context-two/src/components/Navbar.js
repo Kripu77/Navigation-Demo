23694d9f67352679d21106ebb49552d3aea81493
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-25 15:03:53 +1100

    update

--- context-two/src/components/Navbar.js
+++ context-two/src/components/Navbar.js
@@ -1,27 +1,34 @@
 import React ,{useState}from 'react'
 import { data } from './data'
 import Header from './Header';
+import { FaAffiliatetheme } from "react-icons/fa";
 
 const Navbar = () => {
 
     const [navData, setNavData] = useState(data)
     return (
       <main>
+        <Header />
         <div className="wrapper">
-          <Header />
-
           <div className="nav-container">
-            {navData.map((value) => {
-              const { url, id, name } = value;
-              return (
-                <div key={id}>
-                  <a href={url} alt={name}>
-                    {" "}
-                    {url} {name}
-                  </a>{" "}
-                </div>
-              );
-            })}
+            <h1> Kripu Khadka</h1>
+
+            <FaAffiliatetheme />
+          </div>
+          <div className="nav-links">
+            <div className="nav-social">
+              {navData.map((value) => {
+                const { url, id, name } = value;
+                return (
+                  <div key={id}>
+                    <a href={url} alt={name}>
+                      {" "}
+                      {name}
+                    </a>{" "}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </main>
